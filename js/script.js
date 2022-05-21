1809c21e12ccb5ffc374ccb7ed3a1cd05314c1f0
randyazharalman <azharalman17@gmail.com>
2022-05-21 08:38:05 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// event pada saat link di klik
+	$('.page-scroll').on('click', function(e) {
+
+ var tujuan = $(this).attr('href');
+
+ var elemenTujuan = $(tujuan);
+
+ $('html , body').animate({
+  scrollTop: elemenTujuan.offset().top - 50
+ },1000);
+
+ e.preventDefault();
+});
